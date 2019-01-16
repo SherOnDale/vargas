96873c36226572844ec36eed92d8ea47723e5efe
Sherin Binu <sherinbinu@hotmail.com>
2019-01-16 09:06:20 +0530

    add label

--- pages/send.vue
+++ pages/send.vue
@@ -1,9 +1,11 @@
 <template>
   <form>
     <label for="address">
+      Address
       <input id="address" type="text">
     </label>
     <label for="amount">
+      Amount
       <input id="amount" type="text">
     </label>
     <input type="submit" value="Send">
