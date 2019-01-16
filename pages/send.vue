74f7ce37cbcac6dba08a4a8200f622d7ac9cab3a
Sherin Binu <sherinbinu@hotmail.com>
2019-01-16 09:00:39 +0530

    create a send form

---
+++ pages/send.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <label for="address">
+      <input id="address" type="text">
+    </label>
+    <label for="amount">
+      <input id="amount" type="text">
+    </label>
+    <input type="submit" value="Send">
+  </form>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
